3ab434d94da17b41fd2d0b2f55de372badb67dd8
Scaryune <scaryune@gmail.com>
2024-05-22 22:35:21 +0100

    Noctis

--- classes/modify_ai.py
+++ classes/modify_ai.py
@@ -1,27 +1,36 @@
 import json
 
 def modify_ai(file_path, modifier, value):
-    with open(file_path,'r') as json_file:
+    # Read the JSON file
+    with open(file_path, 'r') as json_file:
         ai_profile = json.load(json_file)
 
-        print('Ai profile loaded :')
-        for key, value in ai_profile.items():
-            print(f'{key}: {value}')
+    # Print the loaded AI profile
+    print('AI profile loaded:')
+    for key, val in ai_profile.items():
+        print(f'{key}: {val}')
     
-    if modifier in ai_profile :
-       ai_profile[modifier] = value 
+    # Modify the value of the specified key
+    if modifier in ai_profile:
+        ai_profile[modifier] = value
     else:
         print(f'Key "{modifier}" not found in the AI profile')
-
+        return
+    
+    # Write the modified AI profile back to the JSON file
     with open(file_path, 'w') as json_file:
         json.dump(ai_profile, json_file, indent=4)
-        print('\n Ai profile has updated with sucess')
-    print('\n Modified User Profile:')
-    for key, value in ai_profile.items():
-        print(f'{key}:{value}')
+        print('\nAI profile has been updated successfully')
 
-modifier = input('what key you want change :')
-value = input('Insert the value :')
+    # Print the modified AI profile
+    print('\nModified AI Profile:')
+    for key, val in ai_profile.items():
+        print(f'{key}: {val}')
+
+# Prompt the user for input
+modifier = input('What key do you want to change: ')
+value = input('Insert the value: ')
 file_path = 'ai_profile.json'
 
-ai_profile = modify_ai(file_path, modifier, value)
+# Call the function to modify the AI profile
+modify_ai(file_path, modifier, value)
